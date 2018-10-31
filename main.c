dd2303cd38a856037fcb75e82c006a97863d6087
Felipe Lopes <felipe_lopes@outlook.com>
2018-10-31 10:10:31 -0300

    Adicionado argumentos ao software. Agora é necessário passar o caminho do diretório no executável.

--- main.c
+++ main.c
@@ -4,10 +4,17 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "felipe.h"
 
-int main(void) {
-    ler_dir("/home/felipe/txt");
+int main(int argc, char *argv[]) {
 
-    return 0;
+    if(argc != 2){
+        printf("[ERRO] Eh necessario passar o caminho do diretorio onde o programa fara a leitura dos arquivos\n");
+        return EXIT_FAILURE;
+    } else {
+        ler_dir(argv[1]);
+    }
+
+    return EXIT_SUCCESS;
 }
